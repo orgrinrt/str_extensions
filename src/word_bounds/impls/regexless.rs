34fd8f09ea080ec1df266ba6f0c1b81a3b17e342
orgrinrt <ort@hiisi.digital>
2024-06-27 16:11:58 +0300

    update regexless resolver so that it passes the one test we have currently

--- src/word_bounds/impls/regexless.rs
+++ src/word_bounds/impls/regexless.rs
@@ -7,9 +7,8 @@ impl WordBoundResolverLike for WordBoundResolverRegexless {
     fn resolve(s: &str) -> Vec<String> {
         __str_ext__instance_words_vec!(s, words);
 
-        let mut buffer: String = String::new();
         let mut current_word: String = String::new();
-        let mut prev_is_uppercase = false;
+        let mut prev_char: char = ' ';
 
         for ch in s.chars() {
             match ch {
@@ -18,32 +17,34 @@ impl WordBoundResolverLike for WordBoundResolverRegexless {
                         words.push(current_word.to_lowercase());
                         current_word.clear();
                     }
-                    prev_is_uppercase = false;
                 },
-                ch if ch.is_uppercase() => {
-                    if prev_is_uppercase {
-                        buffer.push(ch);
-                    } else if !current_word.is_empty() {
-                        words.push(current_word.to_lowercase());
-                        current_word.clear();
-                    }
-                    prev_is_uppercase = true;
-                },
-                ch if ch.is_lowercase() => {
-                    if prev_is_uppercase {
-                        words.push(current_word.to_lowercase());
-                        current_word.clear();
-                        if !buffer.is_empty() {
-                            current_word.push_str(&buffer);
-                            buffer.clear();
+                ch if ch.is_alphabetic() => {
+                    if prev_char.is_lowercase() && ch.is_uppercase() {
+                        if !current_word.is_empty() {
+                            words.push(current_word.to_lowercase());
+                            current_word.clear();
                         }
+                    } else if prev_char == '_' || prev_char == '-' {
+                        current_word.clear();
                     }
-                    prev_is_uppercase = false;
+                    current_word.push(ch);
                 },
-                _ => {},
-            }
+                ch if ch.is_numeric() => {
+                    if !current_word.is_empty() {
+                        words.push(current_word.to_lowercase());
+                        current_word.clear();
+                    }
+                    current_word.push(ch);
+                },
+                _ => {
+                    if !current_word.is_empty() {
+                        words.push(current_word.to_lowercase());
+                        current_word.clear();
+                    }
+                },
+            };
 
-            current_word.push(ch);
+            prev_char = ch;
         }
 
         if !current_word.is_empty() {
